995cb205004648b7e218561a6fe88d564569a394
herusanna <61598874+herusanna@users.noreply.github.com>
2020-03-06 14:44:32 +0200

    Update Program.cs

--- laba.net/Program.cs
+++ laba.net/Program.cs
@@ -12,7 +12,7 @@ namespace lab1
             double a = r.Next();
             Console.WriteLine("First number: " + (a));
             Random r1 = new Random();
-            double b = r.Next();
+            double b = r1.Next();
             Console.WriteLine("Second number: " + (b));
             Console.WriteLine("Result =" + (res = task1(a, b)));
             Console.ReadLine();
