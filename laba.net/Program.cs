3b7fe2a9d09492520a52ce7c5503c126e2d9b549
herusanna <61598874+herusanna@users.noreply.github.com>
2020-03-06 14:34:25 +0200

    Добавьте файлы проекта.

---
+++ laba.net/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5var
+            double res;
+            Random r = new Random();
+            double a = r.Next();
+            Console.WriteLine("First number: " + (a));
+            Random r1 = new Random();
+            double b = r.Next();
+            Console.WriteLine("Second number: " + (b));
+            Console.WriteLine("Result =" + (res = task1(a, b)));
+            Console.ReadLine();
+        }
+        static double task1(double x, double y)
+        {
+            double result = x - y;
+            return result;
+        }
+
+    }
+}
